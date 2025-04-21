eacb6ad08f12cdc3677fc133bcac6c2488a8b0db
Bilal Mughal <bilalmughal@Bilals-MacBook-Air.local>
2025-04-21 17:09:36 +0300

    removed unused packages

--- webhook.py
+++ webhook.py
@@ -1,34 +1,8 @@
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
-import hmac
-import hashlib
-import os
-from nylas import Client
-from dataclasses import dataclass
-import pendulum
 
-# Initialize Nylas client
-nylas = Client(
-    api_key = os.getenv('V3_API_KEY')
-)
-
-# Get today’s date
-today = pendulum.now()
-
-# Create the FastAPI app
 app = FastAPI()
 
-# Webhook storage
-webhooks = []
-
-@dataclass
-class Webhook:
-    _id: str
-    date: str
-    title: str
-    description: str
-    participants: str
-    status: str
 
 # Pydantic model for webhook data
 class WebhookData(BaseModel):
